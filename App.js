0ecb6b2171a1b4dabe3f91a90159ac336f527529
Greg Price <gnprice@gmail.com>
2018-06-10 22:46:34 -0700

    Tighten App.js for presentation

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
-
   state = {
     value: '',
   };

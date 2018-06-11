3a3ef40cb36ce944cbf0ab78ae20792daa948301
Greg Price <gnprice@gmail.com>
2018-06-10 23:00:36 -0700

    Tighten App.js further

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { TextInput, View } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -13,7 +13,7 @@ export default class App extends Component<Props> {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <TextInput
            value={this.state.value}
            onChangeText={this.handleTextChange}
@@ -22,12 +22,3 @@ export default class App extends Component<Props> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
